3a4a5c720cbed066fe4b0a0bb34173e61a946670
Maira Francini <mairafrancini@gmail.com>
2022-12-01 17:18:24 -0300

    Alteração no método copyToDomainObject() para evitar HibernateException: identifier of an instance of ? was altered from 1 to 2

--- livros-api/src/main/java/com/maira/livrosapi/api/assembler/LivroInputDisassembler.java
+++ livros-api/src/main/java/com/maira/livrosapi/api/assembler/LivroInputDisassembler.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.maira.livrosapi.api.model.input.LivroInput;
+import com.maira.livrosapi.domain.model.Autor;
+import com.maira.livrosapi.domain.model.Editora;
+import com.maira.livrosapi.domain.model.Genero;
 import com.maira.livrosapi.domain.model.Livro;
 
 @Component
@@ -18,6 +21,14 @@ public class LivroInputDisassembler {
 	}
 
 	public void copyToDomainObject(LivroInput livroInput, Livro livro) {
+		
+		// Para evitar org.hibernate.HibernateException: identifier of an instance of 
+		// com.maira.livrosapi.domain.model.Autor/com.maira.livrosapi.domain.model.Editora/
+		// com.maira.livrosapi.domain.model.Genero was altered from 1 to 2
+		livro.setAutor(new Autor());
+		livro.setEditora(new Editora());
+		livro.setGenero(new Genero());
+		
 		modelMapper.map(livroInput, livro);
 	}
 }
