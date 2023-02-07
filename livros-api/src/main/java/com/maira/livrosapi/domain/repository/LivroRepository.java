d9086a9d2560a9a3b6040e8ce8bbfb624c17f855
Maira Francini <mairafrancini@gmail.com>
2023-02-07 13:54:19 -0300

    Implementação de método para busca de foto pelo código do livro

--- livros-api/src/main/java/com/maira/livrosapi/domain/repository/LivroRepository.java
+++ livros-api/src/main/java/com/maira/livrosapi/domain/repository/LivroRepository.java
@@ -1,15 +1,22 @@
 package com.maira.livrosapi.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.maira.livrosapi.domain.model.FotoLivro;
 import com.maira.livrosapi.domain.model.Livro;
 
 @Repository
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-	
+public interface LivroRepository extends JpaRepository<Livro, Long>, LivroRepositoryQueries {
+
 	Page<Livro> findByTituloContaining(String titulo, Pageable pageable);
+
+	@Query("select f from FotoLivro f join f.livro l where f.livro.id = :livroId")
+	Optional<FotoLivro> findFotoById(Long livroId);
 
 }
