1656a4da01051aa30ac8406ed4ef902502ae48d1
Maira Francini <mairafrancini@gmail.com>
2023-02-09 17:58:07 -0300

    Implementação de rota para cadastrar, alterar e remover foto do livro

--- livros-ui-bootstrap/src/app/livros/livros-routing.module.ts
+++ livros-ui-bootstrap/src/app/livros/livros-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FotoUploadComponent } from './foto-upload/foto-upload.component';
 import { LivroCadastroComponent } from './livro-cadastro/livro-cadastro.component';
 import { LivrosPesquisaComponent } from './livros-pesquisa/livros-pesquisa.component';
 
 const routes: Routes = [
   { path: '', component: LivrosPesquisaComponent },
   { path: 'new', component: LivroCadastroComponent },
-  { path: 'edit/:codigo', component: LivroCadastroComponent }
+  { path: 'edit/:codigo', component: LivroCadastroComponent },
+  { path: 'edit/:codigo/foto', component: FotoUploadComponent }
 ];
 
 @NgModule({
