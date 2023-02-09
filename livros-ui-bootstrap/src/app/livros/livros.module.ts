ef53185bdb90625d4bda816916c4c65c21bef4b8
Maira Francini <mairafrancini@gmail.com>
2023-02-09 18:03:29 -0300

    Inclusão do componente FotoUploadComponent no módulo Livros

--- livros-ui-bootstrap/src/app/livros/livros.module.ts
+++ livros-ui-bootstrap/src/app/livros/livros.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { FotoUploadComponent } from './foto-upload/foto-upload.component';
 import { LivroCadastroComponent } from './livro-cadastro/livro-cadastro.component';
 import { LivrosPesquisaComponent } from './livros-pesquisa/livros-pesquisa.component';
 import { LivrosRoutingModule } from './livros-routing.module';
@@ -11,7 +12,8 @@ import { LivrosRoutingModule } from './livros-routing.module';
 @NgModule({
   declarations: [
     LivrosPesquisaComponent,
-    LivroCadastroComponent
+    LivroCadastroComponent,
+    FotoUploadComponent
   ],
   imports: [
     CommonModule,
