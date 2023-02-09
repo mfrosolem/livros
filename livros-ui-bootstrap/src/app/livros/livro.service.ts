9f4606268d769f1f9a15ca0ef810610eac7bfae6
Maira Francini <mairafrancini@gmail.com>
2023-02-09 17:59:35 -0300

    Implementação de métodos para acessar Rest API para cadastrar, alterar e remover foto

--- livros-ui-bootstrap/src/app/livros/livro.service.ts
+++ livros-ui-bootstrap/src/app/livros/livro.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, take } from 'rxjs';
+import { map, Observable, take } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 import { Livro } from './../core/models/model';
@@ -70,15 +70,42 @@ export class LivroService {
   }
 
   save(record: Partial<Livro>) {
+
     if (record.id) {
       return this.update(record);
     } else {
       return this.create(record);
     }
+
   }
 
   remove(codigo: number) {
     return this.http.delete(`${this.API}/${codigo}`).pipe(take(1));
+  }
+
+  getFoto(codigo: number) {
+
+    let headers = new HttpHeaders().set('Accept', "image/jpeg");
+
+    return this.http.get(`${this.API}/${codigo}/foto`, { headers, responseType: 'blob' }).pipe(take(1));
+
+  }
+
+  uploadFoto(livro: Livro, foto: File) {
+    const url = `${this.API}/${livro.id}/foto`;
+
+    const formData = new FormData();
+    formData.append('arquivo', foto);
+    formData.append('descricao', `Capa_${livro.id}`);
+
+    return this.http.put(url, formData).pipe(take(1));
+
+  }
+
+  removeFoto(codigo: number) {
+    const url = `${this.API}/${codigo}/foto`;
+
+    return this.http.delete(url).pipe(take(1));
   }
 
   private create(record: Partial<Livro>) {
